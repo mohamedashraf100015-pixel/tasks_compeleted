408103e137f02fff8a93af7d057e27401bfe452f
Mohamed Ashraf <mohamedashraf10015@gmail.com>
2026-09-17 19:06:39 +0300

    #edit errors in build.gradle.kts and CMakeLists.txt linux and windows

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+
         applicationId = "com.example.tasks_compeleted"
         minSdk = flutter.minSdkVersion
         // You can update the following values to match your application needs.
@@ -32,7 +32,7 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
+
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
